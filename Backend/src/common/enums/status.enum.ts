abe68c854f347f2ac3e6ac9df1dffdcb5dc01c91
LoD-Heba <zegion511@gmail.com>
2025-10-12 13:36:11 -0700

    emuns globales

---
+++ Backend/src/common/enums/status.enum.ts
@@ -0,0 +1,33 @@
+export enum TripStatus {
+  SCHEDULED = 'SCHEDULED',
+  IN_PROGRESS = 'IN_PROGRESS',
+  COMPLETED = 'COMPLETED',
+  CANCELLED = 'CANCELLED',
+}
+
+export enum TicketStatus {
+  CONFIRMED = 'CONFIRMADO',
+  PENDING = 'PENDIENTE',
+  CANCELLED = 'CANCELADO',
+}
+// ====== PAYMENT ======
+export enum PaymentStatus {
+  PENDING = 'PENDIENTE',
+  COMPLETED = 'COMPLETO',
+  FAILED = 'FALLIDO',
+  REFUNDED = 'REEMBOLZO',
+}
+
+export enum PaymentMethod {
+  CASH = 'EFECTIVO',
+  CARD = 'TARJETA',
+  QR = 'QR',
+  TRANSFER = 'TRANSFERENCIA',
+}
+
+export enum PaymentCategory {
+  CHILD = 'niño',
+  ADULT = 'adulto',
+  SENIOR = 'adulto_mayor',
+  STUDENT = 'estudiante',
+}
