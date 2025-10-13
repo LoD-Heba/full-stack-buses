e066099f2fa41af9d629b27edfbd0193e0b453f5
LoD-Heba <zegion511@gmail.com>
2025-10-12 22:00:49 -0700

    Solo moficicamos el finOne para que muestre el bus en el create

--- Backend/src/modules/client/seat-stacks/seat-stacks.service.ts
+++ Backend/src/modules/client/seat-stacks/seat-stacks.service.ts
@@ -1,32 +1,41 @@
-import { Injectable, NotFoundException} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { SeatStack } from './entities/seat-stack.entity';
 import { CreateSeatStackDto } from './dto/create-seat-stack.dto';
 import { UpdateSeatStackDto } from './dto/update-seat-stack.dto';
+import { Bus } from '../bus/entities/bus.entity';
 
 @Injectable()
 export class SeatStacksService {
   constructor(
     @InjectRepository(SeatStack)
     private readonly seatStackRepository: Repository<SeatStack>,
+
+    @InjectRepository(Bus)
+    private readonly busRepository: Repository<Bus>,
   ) {}
   async create(createSeatStackDto: CreateSeatStackDto) {
-    const seatStack = this.seatStackRepository.create(createSeatStackDto);
-    return this.seatStackRepository.save(seatStack);
+    const { ...stackData } = createSeatStackDto;
+
+    const seatStack = this.seatStackRepository.create({ ...stackData });
+
+    const newStack = this.seatStackRepository.save(seatStack);
+    return this.findOne((await newStack).id);
   }
 
   async findAll() {
-    const data = this.seatStackRepository.find(
-      {
-        relations:{seats: true}
-      }
-    );
-    return data
+    const data = this.seatStackRepository.find({
+      relations: { seats: true, bus: true },
+    });
+    return data;
   }
 
   async findOne(id: string) {
-    const seatStack = await this.seatStackRepository.findOne({ where: { id }, relations:{seats:true} });
+    const seatStack = await this.seatStackRepository.findOne({
+      where: { id },
+      relations: { seats: true, bus: true },
+    });
     if (!seatStack) {
       throw new NotFoundException(`El stack ${id} no ha sido encontrado`);
     }
@@ -42,6 +51,6 @@ export class SeatStacksService {
   async remove(id: string) {
     const deleteStack = await this.findOne(id);
     await this.seatStackRepository.remove(deleteStack);
-    return('El stack fue eliminado correctamente')
+    return 'El stack fue eliminado correctamente';
   }
 }
